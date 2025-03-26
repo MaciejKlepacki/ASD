3e3f19f5534a27100bb623765bd33edfca82246c
MaciejKlepacki <maciek.klepacki@icloud.com>
2025-03-26 21:29:47 +0100

    Adding some sorting algorithms

--- part_1/Algorytmy/descending_merge_sort.py
+++ part_1/Algorytmy/descending_merge_sort.py
@@ -1,5 +1,4 @@
-# Złozoność czasowa: O(nlogn)
-# Złozoność pamięciowa: O(n)
+# Złozoność czasowa: O(n*log(n))
 
 def merge_sort(T):
     if len(T) <= 1:
@@ -7,23 +6,26 @@ def merge_sort(T):
     mid = len(T)//2
     left = merge_sort(T[:mid])
     right = merge_sort(T[mid:])
-    return merge(left, right)
+    return descending_merge(left, right)
 
-def merge(left, right):
+def descending_merge(left, right):
+    # Prealokujemy listę o ustalonym rozmiarze
     result = [None] * (len(left) + len(right))
     l = r = index = 0
     while l < len(left) and r < len(right):
-        if left[l] <= right[r]:
+        if left[l] >= right[r]:  # Zmieniono warunek na malejący porządek
             result[index] = left[l]
             l += 1
         else:
             result[index] = right[r]
             r += 1
         index += 1
+    # Przepisujemy pozostałe elementy lewej tablicy
     while l < len(left):
         result[index] = left[l]
         l += 1
         index += 1
+    # Przepisujemy pozostałe elementy prawej tablicy
     while r < len(right):
         result[index] = right[r]
         r += 1
